9ff672a46c2267a0ed53d60db9291f2385e66854
architvohra123 <archit0249.be21@chitkara.edu.in>
2022-12-26 17:09:17 +0530

    1st feature added in feature 1

--- sam3.cpp
+++ sam3.cpp
@@ -4,4 +4,5 @@ int main(){
     cout<<"this is sample code 3";
     cout<<" this is also new line added";
     cout<<"3 rd";
+    cout<<"This is feature1jysdghkjsbjvshlckns djhvbsukjcbv sdjvukhsndb cnskdicsn cvhjusdjbc nscb snmc hs cmjns dnc snmd";
 }
